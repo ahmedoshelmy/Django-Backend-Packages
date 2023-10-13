81ad00cbb50cbb19743ced45ca175ac0614e1c68
Ahmed Osama Blue <a.osama@blue-developments.team>
2023-10-13 16:10:44 +0300

    Finished

--- packagesAPIs/settings.py
+++ packagesAPIs/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,11 +80,11 @@ WSGI_APPLICATION = 'packagesAPIs.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Packages_Django',
-        'USER': 'postgres',
-        'PASSWORD': 'OsOs1982',
-        'HOST': '127.0.0.1',
-        'PORT': '5432'
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
